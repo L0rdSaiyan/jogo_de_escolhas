f0b06d79b74ca92f0b35142e2eab845878f59275
joao victor <thehoodgamer20@gmail.com>
2022-07-28 02:11:24 -0300

    commit initial

---
+++ app.js
@@ -0,0 +1,43 @@
+let resposta = document.getElementById("option1");
+let resposta2 = document.getElementById("option2");
+
+resposta.onclick = function(){
+
+    alert("clicou no "+resposta.innerText)
+    resposta.innerHTML = 'option 1 nova'
+}
+
+resposta2.onclick = function(){
+
+alert("clicou no "+resposta2.innerText);
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+/*
+function mudarTexto(){
+
+if(resposta.click){
+
+alert("clicou no "+resposta.innerText)
+
+}else if(resposta2.click) {
+
+ alert("clicou no "+resposta2.innerText)
+
+}
+
+
+
+}
+*/
