22837e4fe2f03853cf8c9306f9b3cfbe8842df88
joao victor <thehoodgamer20@gmail.com>
2022-08-01 01:04:51 -0300

    adicionado mais perguntas e funcionalidades

--- app.js
+++ app.js
@@ -1,15 +1,45 @@
 let resposta = document.getElementById("option1");
 let resposta2 = document.getElementById("option2");
+let content2 = document.createTextNode("frio")
+let content3 = document.createTextNode("calor")
+
+let clicks = 0
 
 resposta.onclick = function(){
 
+    clicks = clicks+1
+
+if(clicks==1){
+
     alert("clicou no "+resposta.innerText)
-    resposta.innerHTML = 'option 1 nova'
+    resposta.innerText = 'frio'
+    resposta2.innerText = 'calor'
+    console.log(clicks)
+    clicks=clicks++
+
+}
+else if(clicks==2){
+
+    alert(`Clicou no ${resposta.innerText}`)
+    resposta.innerText = 'Filme'
+    resposta2.innerText = 'Livro'
+    clicks++
+
+}
+else if(clicks==3){
+
+    alert(`Clicou no ${resposta.innerText}`)
+
+
+}
+
 }
 
 resposta2.onclick = function(){
 
-alert("clicou no "+resposta2.innerText);
+    alert("clicou no "+resposta2.innerText);
+    resposta.innerHTML = 'frio'
+    resposta2.innerHTML = 'calor'
 
 
 }
